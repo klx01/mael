4d5ba053cde1472703a81bc5873b719b2dedba71
klx01 <69247896+klx01@users.noreply.github.com>
2024-04-14 15:28:46 +0400

    allow concurrent writes to existing keys

--- src/bin/txn_async.rs
+++ src/bin/txn_async.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
+use std::time::Duration;
 use serde::{Deserialize, Serialize};
+use tokio::sync::RwLock;
 use mael::async_service::{AsyncService, default_init_and_async_loop};
 use mael::id_generator::MessageIdGenerator;
 use mael::init::DefaultInitService;
@@ -26,14 +28,15 @@ struct TxnOkMessage {
 }
 
 enum StatementError {
-    InvalidAction,
+    InvalidAction(char),
     WriteNone,
+    LockWaitTimeout,
 }
 
 struct TxnService {
     id: MessageIdGenerator,
     node_id: String,
-    storage: RwLock<HashMap<usize, usize>>,
+    storage: RwLock<HashMap<usize, RwLock<Option<usize>>>>,
 }
 impl DefaultInitService for TxnService {
     fn new(init_message: InitMessage) -> Self {
@@ -52,7 +55,7 @@ impl TxnService {
                 if let Some(val) = changes.get(key).copied() {
                     Ok(Statement(*action, *key, Some(val)))
                 } else {
-                    Ok(Statement(*action, *key, self.read_value(key)))
+                    Ok(Statement(*action, *key, self.read_value(key).await))
                 }
             },
             'w' => match value {
@@ -62,29 +65,91 @@ impl TxnService {
                 },
                 None => Err(StatementError::WriteNone),
             },
-            _ => Err(StatementError::InvalidAction),
+            _ => Err(StatementError::InvalidAction(*action)),
         }
     }
-    fn read_value(&self, key: &usize) -> Option<usize> {
-        let lock = self.storage.read().expect("got a poisoned lock, cant really handle it");
-        let val = lock.get(key).copied();
-        drop(lock); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
-        val
+    async fn read_value(&self, key: &usize) -> Option<usize> {
+        let storage_guard = self.storage.read().await;
+        let value_lock = storage_guard.get(key);
+
+        let Some(value_lock) = value_lock else {
+            drop(storage_guard);
+            return None;
+        };
+        let value_guard = value_lock.read().await;
+        let value = *value_guard;
+        drop(value_guard);
+        drop(storage_guard); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+
+        value
     }
-    fn flush_writes(&self, changes: HashMap<usize, usize>) {
-        let mut lock = self.storage.write().expect("got a poisoned lock, cant really handle it");
+    async fn flush_writes(&self, changes: HashMap<usize, usize>) -> Result<(), StatementError> {
+        if changes.len() == 0 {
+            return Ok(());
+        }
+        /*
+        most of the complexity comes from trying to achieve this:
+            1) don't take a global write lock for all writes
+            2) if 2 concurrent transactions are writing into the same set of keys at the same time, we should see results only of one of them.
+                we should not see values from the 1st one in one part of keys, and values from 2nd one for another part of keys
+            3) given both of the above, deadlocks are possible. In case of a deadlock, one tx should be rolled back completely
+         */
+        self.init_new_keys(self.get_new_keys(&changes).await).await;
+
+        let storage_guard = self.storage.read().await;
+        let mut value_guards = HashMap::new();
+        for key in changes.keys() {
+            let value_lock = storage_guard.get(key).expect("values should be present for all keys, the call to init_new_keys() above should take care of that");
+            let value_guard_future = value_lock.write();
+            let value_guard = tokio::time::timeout(
+                Duration::from_millis(50),
+                value_guard_future,
+            ).await;
+            let Ok(value_guard) = value_guard else {
+                drop(value_guard);
+                drop(value_guards);
+                drop(storage_guard);
+                return Err(StatementError::LockWaitTimeout);
+            };
+            value_guards.insert(*key, value_guard);
+        }
         for (key, value) in changes {
-            lock.insert(key, value);
+            let value_guard = value_guards.get_mut(&key).unwrap();
+            **value_guard = Some(value);
         }
-        drop(lock); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+
+        drop(value_guards); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+        drop(storage_guard);
+        Ok(())
     }
-    fn handle_error(stmt: &Statement, error: StatementError, msg_id: usize, meta: MessageMeta) {
+    async fn get_new_keys(&self, changes: &HashMap<usize, usize>) -> Vec<usize> {
+        let storage_guard = self.storage.read().await;
+        let res = changes
+            .keys()
+            .filter(|key| !storage_guard.contains_key(*key))
+            .copied()
+            .collect();
+        drop(storage_guard); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+        res
+    }
+    async fn init_new_keys(&self, keys: Vec<usize>) {
+        if keys.len() == 0 {
+            return;
+        }
+        let mut storage_guard = self.storage.write().await;
+        for key in keys {
+            // if the key was inserted between collecting the list and taking the write lock, we need to make sure to not overwrite it yet, because current tx can be rolled back
+            storage_guard.entry(key).or_insert(RwLock::new(None));
+        }
+        drop(storage_guard); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+    }
+    fn handle_error(error: StatementError, msg_id: usize, meta: MessageMeta) {
         match error {
-            StatementError::InvalidAction => {
+            StatementError::InvalidAction(action) => {
                 let error = ErrorMessage {
                     in_reply_to: msg_id,
                     code: ErrorCode::MalformedRequest,
-                    text: format!("invalid action {}", stmt.0),
+                    text: format!("invalid action {action}"),
                 };
                 output_reply(error, meta);
             },
@@ -93,6 +158,14 @@ impl TxnService {
                     in_reply_to: msg_id,
                     code: ErrorCode::MalformedRequest,
                     text: "empty write value".to_string(),
+                };
+                output_reply(error, meta);
+            },
+            StatementError::LockWaitTimeout => {
+                let error = ErrorMessage {
+                    in_reply_to: msg_id,
+                    code: ErrorCode::TxnConflict,
+                    text: "lock wait timeout".to_string(),
                 };
                 output_reply(error, meta);
             },
@@ -110,12 +183,16 @@ impl AsyncService<TxnMessage> for TxnService {
             match stmt_response {
                 Ok(resp) => response.push(resp),
                 Err(err) => {
-                    Self::handle_error(&stmt, err, message.msg_id, meta);
+                    Self::handle_error(err, message.msg_id, meta);
                     return;
                 }
             }
         }
-        arc_self.flush_writes(changes);
+        let flush_result = arc_self.flush_writes(changes).await;
+        if let Err(err) = flush_result {
+            Self::handle_error(err, message.msg_id, meta);
+            return;
+        }
 
         let output = TxnOkMessage {
             msg_id: arc_self.id.next(),
