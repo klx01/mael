9c23f13b8b96e9c8b94c84c5572e2cb07ba8fe7a
klx01 <69247896+klx01@users.noreply.github.com>
2024-04-14 13:55:11 +0400

    allow parallel reads with read committed model

--- src/bin/txn_async.rs
+++ src/bin/txn_async.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use serde::{Deserialize, Serialize};
 use mael::async_service::{AsyncService, default_init_and_async_loop};
 use mael::id_generator::MessageIdGenerator;
@@ -25,10 +25,15 @@ struct TxnOkMessage {
     txn: Vec<Statement>,
 }
 
+enum StatementError {
+    InvalidAction,
+    WriteNone,
+}
+
 struct TxnService {
     id: MessageIdGenerator,
     node_id: String,
-    storage: Mutex<HashMap<usize, usize>>,
+    storage: RwLock<HashMap<usize, usize>>,
 }
 impl DefaultInitService for TxnService {
     fn new(init_message: InitMessage) -> Self {
@@ -39,54 +44,88 @@ impl DefaultInitService for TxnService {
         }
     }
 }
+impl TxnService {
+    async fn process_statement(&self, stmt: &Statement, changes: &mut HashMap<usize, usize>) -> Result<Statement, StatementError> {
+        let Statement(action, key, value) = stmt;
+        match action {
+            'r' => {
+                if let Some(val) = changes.get(key).copied() {
+                    Ok(Statement(*action, *key, Some(val)))
+                } else {
+                    Ok(Statement(*action, *key, self.read_value(key)))
+                }
+            },
+            'w' => match value {
+                Some(value) => {
+                    changes.insert(*key, *value);
+                    Ok(Statement(*action, *key, Some(*value)))
+                },
+                None => Err(StatementError::WriteNone),
+            },
+            _ => Err(StatementError::InvalidAction),
+        }
+    }
+    fn read_value(&self, key: &usize) -> Option<usize> {
+        let lock = self.storage.read().expect("got a poisoned lock, cant really handle it");
+        let val = lock.get(key).copied();
+        drop(lock); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+        val
+    }
+    fn flush_writes(&self, changes: HashMap<usize, usize>) {
+        let mut lock = self.storage.write().expect("got a poisoned lock, cant really handle it");
+        for (key, value) in changes {
+            lock.insert(key, value);
+        }
+        drop(lock); // dropping lock guards explicitly to be future-proof. i.e. if i want to add something to the end of the function, i would need make a conscious decision for adding it before or after drop
+    }
+    fn handle_error(stmt: &Statement, error: StatementError, msg_id: usize, meta: MessageMeta) {
+        match error {
+            StatementError::InvalidAction => {
+                let error = ErrorMessage {
+                    in_reply_to: msg_id,
+                    code: ErrorCode::MalformedRequest,
+                    text: format!("invalid action {}", stmt.0),
+                };
+                output_reply(error, meta);
+            },
+            StatementError::WriteNone => {
+                let error = ErrorMessage {
+                    in_reply_to: msg_id,
+                    code: ErrorCode::MalformedRequest,
+                    text: "empty write value".to_string(),
+                };
+                output_reply(error, meta);
+            },
+        }
+    }
+}
 impl AsyncService<TxnMessage> for TxnService {
-    async fn process_message(arc_self: Arc<Self>, mut message: TxnMessage, meta: MessageMeta) {
-        // todo: try to implement this without having a global mutex for the whole transaction
-        let mut storage = arc_self.storage.lock().expect("got a poisoned lock, cant really handle it");
-        for stmt in message.txn.iter_mut() {
-            let Statement(action, key, value) = stmt;
-            match action {
-                'r' => {
-                    *value = storage.get(key).copied();
-                },
-                'w' => match value {
-                    Some(value) => {
-                        storage.insert(*key, *value);
-                    },
-                    None => {
-                        drop(storage);
-                        let error = ErrorMessage {
-                            in_reply_to: message.msg_id,
-                            code: ErrorCode::MalformedRequest,
-                            text: "empty write value".to_string(),
-                        };
-                        output_reply(error, meta);
-                        return;
-                    },
-                },
-                _ => {
-                    drop(storage);
-                    let error = ErrorMessage {
-                        in_reply_to: message.msg_id,
-                        code: ErrorCode::MalformedRequest,
-                        text: format!("invalid action {action}"),
-                    };
-                    output_reply(error, meta);
+    async fn process_message(arc_self: Arc<Self>, message: TxnMessage, meta: MessageMeta) {
+        // try to process each statement in the transaction separately from each other, as if they are issued one by one and not as a batch
+        let mut response = vec![];
+        let mut changes = HashMap::new();
+
+        for stmt in message.txn {
+            let stmt_response = arc_self.process_statement(&stmt, &mut changes).await;
+            match stmt_response {
+                Ok(resp) => response.push(resp),
+                Err(err) => {
+                    Self::handle_error(&stmt, err, message.msg_id, meta);
                     return;
-                },
+                }
             }
         }
-        drop(storage);
-        
+        arc_self.flush_writes(changes);
+
         let output = TxnOkMessage {
             msg_id: arc_self.id.next(),
             in_reply_to: message.msg_id,
-            txn: message.txn,
+            txn: response,
         };
         output_reply(output, meta);
     }
 
-    async fn on_timeout(arc_self: Arc<Self>) {
+    async fn on_timeout(_arc_self: Arc<Self>) {
         // empty
     }
 }
@@ -96,12 +135,6 @@ async fn main() {
     // cargo build --release && ./maelstrom/maelstrom test -w txn-rw-register --bin ./target/release/txn_async --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
     // cargo build --release && ./maelstrom/maelstrom test -w txn-rw-register --bin ./target/release/txn_async --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-uncommitted --availability total --nemesis partition
     // cargo build --release && ./maelstrom/maelstrom test -w txn-rw-register --bin ./target/release/txn_async --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-committed --availability total –-nemesis partition
-    
-    /*
-    todo:
-        6c passes even with this implementation. This is probably not intended, but i'm not sure what is intended
-        looks like it is not even required to replicate the writes between nodes, and it is not required for nodes to eventually end up in the same state  
-     */
 
     /*
 {"id":3,"src":"c3","dest":"n1","body":{"type":"init","node_id":"n1","node_ids":["n1","n2","n3","n4","n5"],"msg_id":100}}
